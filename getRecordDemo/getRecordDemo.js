9b197eb6afbfa9473a6997d506a31e5c3a129f0f
tooshort <achernih@yandex.ru>
2021-11-14 23:33:37 +0300

    Add files via upload

---
+++ getRecordDemo/getRecordDemo.js
@@ -0,0 +1,28 @@
+import { LightningElement, wire, api } from 'lwc';
+import { getRecord } from 'lightning/uiRecordApi';
+import NAME_FIELD from '@salesforce/schema/Account.Name';
+import OWNER_NAME_FIELD from '@salesforce/schema/Account.Owner.Name';
+import ANNUAL_REVENUE_FIELD from '@salesforce/schema/Account.AnnualRevenue';
+
+export default class GetRecordDemo extends LightningElement {
+
+    name;
+    owner;
+    annualRevenue;
+
+    @api recordId;
+
+    // @wire(getRecord, {recordId: '$recordId', 
+    //     fields: [NAME_FIELD, OWNER_NAME_FIELD, ANNUAL_REVENUE_FIELD]}) 
+
+    @wire(getRecord, {recordId: '$recordId', 
+        layoutTypes:['Full'], modes:['View']})
+        accountHandler({data}) {
+           if(data) {
+                console.log(data);
+                this.name = data.fields.Name.displayValue ? data.fields.Name.displayValue : data.fields.Name.value;
+                this.annualRevenue = data.fields.AnnualRevenue.displayValue ? data.fields.AnnualRevenue.displayValue : data.fields.AnnualRevenue.value;
+                this.owner = data.fields.Owner.displayValue ? data.fields.Owner.displayValue : data.fields.Owner.value;
+        }
+    }
+}
